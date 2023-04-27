effa1f67bf7ae58b0fbac17afeeb2a7884f54c2b
NicolasSIO <nicolas.mazaud.sio@gmail.com>
2023-04-27 17:52:08 +0200

    Create Layout

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "../components/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const Layout = () => {
-  return <div className="Layout">Layout</div>;
+  return (
+    <div className="Layout">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
