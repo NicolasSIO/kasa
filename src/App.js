b665f3956ef72adcaaad925f9777f0f6cb60af9d
NicolasSIO <nicolas.mazaud.sio@gmail.com>
2023-04-27 17:51:20 +0200

    Create router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "@/App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "@/pages/Layout";
+import Home from "@/pages/Home";
+import APropos from "@/pages/APropos";
+import Error from "./pages/Error";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Home />} />
+
+            <Route path="/accueil" element={<Home />} />
+            <Route path="/a-propos" element={<APropos />} />
+
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
