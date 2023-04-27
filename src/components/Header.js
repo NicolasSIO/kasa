2403ce2ae4b41b7a99ca97a05daf9a45696e1145
NicolasSIO <nicolas.mazaud.sio@gmail.com>
2023-04-27 18:16:26 +0200

    Header implementation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./header.css";
 
 const Header = () => {
-  return <div className="Header">Header</div>;
+  return (
+    <header className="Header">
+      <nav>
+        <img src="./assets/img/logo.svg" alt="Logo Kasa" className="logo" />
+        <ul>
+          <li>
+            <Link to="accueil">Accueil</Link>
+          </li>
+          <li>
+            <Link to="a-propos">A Propos</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
