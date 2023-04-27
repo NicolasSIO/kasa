df531d775b2d1f3d1db2e5286e6a890e39e81c72
NicolasSIO <nicolas.mazaud.sio@gmail.com>
2023-04-27 18:40:02 +0200

    Footer implementation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import "./footer.css";
+
 const Footer = () => {
-  return <div className="Footer">Footer</div>;
+  return (
+    <div className="Footer">
+      <img
+        src="./assets/img/logo.svg"
+        alt="Logo Kasa"
+        className="footer-logo"
+      />
+      <span className="footer-text">© 2020 Kasa. All rights reserved</span>
+    </div>
+  );
 };
 
 export default Footer;
