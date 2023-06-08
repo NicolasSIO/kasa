cc91a2f51635b1e86fc2415aa53e0fcf395296fb
NicolasSIO <nicolas.mazaud.sio@gmail.com>
2023-06-08 12:28:46 +0200

    Link card fix

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,18 +1,19 @@
 import React from "react";
 
 import "./card.css";
+import { Link } from "react-router-dom";
 
 const Card = (props) => {
   return (
     <article className="Card">
-      <a href={"/logement/" + props.id}>
+      <Link to={"/logement/" + props.id}>
         <figure>
           <img src={props.cover} alt={props.title} className="logement-cover" />
           <footer className="figure-footer">
             <p className="logement-title">{props.title}</p>
           </footer>
         </figure>
-      </a>
+      </Link>
     </article>
   );
 };
